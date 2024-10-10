fb24cd8ff1af62ca6970ad5d70112585ef26b14d
Chaima <1925046@alu.murciaeduca.es>
2024-10-10 09:01:53 +0200

    nombre cambiadoooo y codigo perfeccionado con jar y todo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,35 +18,30 @@ public class Main {
             int contador = 0;//para contar el numero de primos que van saliendo
 
             Random random = new Random();
-            //random.nextInt((max - min) + 1) + min para que no se cuente solo entre el2 y 999 incluidos
+            //random.nextInt((max - min) + 1) + min para que se cuente solo entre el 2 y 999 incluidos
 
-            //compruebo si es primo
+
             while (contador < totNumeros)//mientras que no se hayan generado todos los numeros
             {
-                int numero = random.nextInt(((999-2)+1)+2);
-                //pongo el while, porq solo con la formula me ha salido un 0
-                while(numero < 2 || numero > 999)//me aseguro de que este entre ese rango si o si
+                int numero = random.nextInt((999-2)+1)+2;//genero el numero random
+
+                if (esPrimo(numero))//compruebo si es primo
                 {
-                    numero = random.nextInt(((999-2)+1)+2);
-                }
-                if (esPrimo(numero))
-                {
-                    System.out.println(numero);
-                    contador++;
+                    System.out.println(numero);//lo enseño
+                    contador++;//aumento contador
                 }
             }
 
         }else {
             System.out.println("NO has introducido ningun argumento");
         }
-
-
     }
+
     public static boolean esPrimo(int numero)
     {
-        //empiezo a comprobar desde el 2
-        //hasta la raiz cuadrada del numero generado
-        for (int i = 2; i <= Math.sqrt(numero) ; i++) {
+        //empiezo a comprobar desde el 2, el primer num primo
+        //hasta la raiz cuadrada del numero generado, para asi poder hacer menos comprobaciones
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
             if (numero % i == 0)//no es primo
             {
                 return false;
